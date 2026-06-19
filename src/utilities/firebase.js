d1df84593f949a4936836d001e8513bb7b801b77
Carlos Bastida <bastidacarlos996@gmail.com>
2026-06-19 00:50:04 -0600

    Arreglando alertas de seguridad de github

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -5,13 +5,13 @@ import { getAuth, GoogleAuthProvider, onIdTokenChanged, signInWithPopup, signOut
 import { useAuthState } from 'react-firebase-hooks/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAetaV5Domca0_CiUoNB29Rgz7_fcvMoqo",
-  authDomain: "quick-react-scheduler-3b1ab.firebaseapp.com",
-  databaseURL: "https://quick-react-scheduler-3b1ab-default-rtdb.firebaseio.com",
-  projectId: "quick-react-scheduler-3b1ab",
-  storageBucket: "quick-react-scheduler-3b1ab.firebasestorage.app",
-  messagingSenderId: "1023076579041",
-  appId: "1:1023076579041:web:3e7a9aea488575f4ee69a0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const firebase = initializeApp(firebaseConfig);
